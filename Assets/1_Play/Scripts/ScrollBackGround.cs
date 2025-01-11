a3e8ac9c8cdcde59d130c9d36dd2dd7597f1c911
Disaster <Disaster20031017@gmail.com>
2025-01-11 14:01:16 +0900

    Update Background

---
+++ Assets/1_Play/Scripts/ScrollBackGround.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBackGround : MonoBehaviour
+{
+    private Material matBackground;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // コンポーネントの取得
+        matBackground = GetComponent<SpriteRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 背景のスクロール
+        Vector2 offset = matBackground.mainTextureOffset + new Vector2(0.1f * Time.deltaTime, 0);
+        matBackground.SetTextureOffset("_MainTex", offset);
+    }
+}
